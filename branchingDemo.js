94f55662a8ca3ef92bdfccd1c901cb613b097729
juliansanchez1105 <75316436+juliansanchez1105@users.noreply.github.com>
2022-09-09 12:24:36 -0400

    Create branchingDemo.js
    Added the branchingDemo.js file.

---
+++ branchingDemo.js
@@ -0,0 +1,13 @@
+// Filename:    branchingDemo.js
+//Author:       J. Sanchez
+//Objective:    Demonstrate how to use GitHub branches
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August",
+                 "September", "October", "November", "December"];
+
+const d = new Date();
+
+//getMonth() returns the month as an integer (0 - 11). Jan = 0, Dec = 11
+let month = months[d.getMonth()];
+
+console.log("The date is: " + d);
